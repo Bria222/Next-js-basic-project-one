dc4727c563e37c8dcd32d240e13c76a9a4de97de
Bria222 <briantechologies.co.ke@gmail.com>
2023-05-16 01:53:56 +0300

    fetch repos

--- app/code/page.jsx
+++ app/code/page.jsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa'
 async function fetchRepos() {
   const response = await fetch(`https://api.github.com/users/Bria222/repos`)
   const repos = await response.json()
@@ -8,7 +10,16 @@ const ReposPage = async () => {
 
   return (
     <>
-      <h1>{repos[0].name}</h1>
+      <h2>Repositories</h2>
+      <ul class='list-group'>
+        {repos.map((repo) => {
+          return (
+            <li class='list-group-item' key={repo.id}>
+              {repo.name}
+            </li>
+          )
+        })}
+      </ul>
     </>
   )
 }
